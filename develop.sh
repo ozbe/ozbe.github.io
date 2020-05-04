7a586641ab88a30eb9cf2af48d0be204577a51ea
ozbe <1372945+ozbe@users.noreply.github.com>
2020-05-04 23:44:38 +0000

    start docker from run.sh; run gatsby develop from develop.sh

--- develop.sh
+++ develop.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it \
--p 9000:9000 -p 8000:8000 \
--e GATSBY_TELEMETRY_DISABLED=1 \
--v `pwd`:/site \
--v ~/.gitconfig:/root/.gitconfig \
--v ~/.vimrc:/root/.vimrc \
-ozbe/gatsby
+gatsby develop -H 0.0.0.0
