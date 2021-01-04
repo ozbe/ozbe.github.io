278a351c8d73f81363c2b266244531db4fbcc8e4
ozbe <1372945+ozbe@users.noreply.github.com>
2021-01-04 12:03:19 +1100

    tweak colors to darker blues

--- src/gatsby-theme-blog/gatsby-plugin-theme-ui/colors.js
+++ src/gatsby-theme-blog/gatsby-plugin-theme-ui/colors.js
@@ -7,19 +7,20 @@ import defaultThemeColors from "gatsby-theme-blog/src/gatsby-plugin-theme-ui/col
  * to go from default purple to a blue theme
  */
 
-const darkBlue = `#007acc`
-const lightBlue = `#66E0FF`
-const blueGray = `#282c35`
+const midnightBlue = `#1b262c`
+const darkBlue = `#0f4c75`
+const lightBlue = `#3282b8`
+const blueGray = `#bbe1fa`
 
 export default merge(defaultThemeColors, {
-  text: blueGray,
   primary: darkBlue,
-  heading: blueGray,
+  heading: darkBlue,
+  highlight: blueGray,
   modes: {
     dark: {
-      background: blueGray,
+      background: midnightBlue,
       primary: lightBlue,
-      highlight: lightBlue,
+      highlight: darkBlue,
     },
   },
 })
