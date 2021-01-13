d88b898cf2d8e38fde36a42298a5ec5bf114edeb
Josh Aaseby <1372945+ozbe@users.noreply.github.com>
2021-01-13 14:18:12 +1100

    remove any previous utterances elements before adding new elements (#36)

--- src/gatsby-theme-blog/components/post.js
+++ src/gatsby-theme-blog/components/post.js
@@ -32,11 +32,18 @@ const Post = ({
     scriptEl.src = 'https://utteranc.es/client.js'
     scriptEl.setAttribute('repo', 'ozbe/ozbe.github.io')
     scriptEl.setAttribute('issue-term', 'title')
-    scriptEl.setAttribute('id', 'utterances')
+    scriptEl.setAttribute('id', 'blog-comment')
     scriptEl.setAttribute('theme', 'github-light')
     scriptEl.setAttribute('crossorigin', 'anonymous')
     if (commentBox && commentBox.current) {
-      commentBox.current.appendChild(scriptEl)
+      const node = commentBox.current
+
+      // Remove previous utterances comments
+      while (node.firstChild) {
+        node.removeChild(node.lastChild)
+      }
+      
+      node.appendChild(scriptEl, scriptEl)
     } else {
       console.log(`Error adding utterances comments on: ${commentBox}`)
     }
